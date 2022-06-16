7a066cf4ac8ec5c8bcc99ada57bdcb41c084f9c3
Gulzhigit <mr.ksedos@gmail.com>
2022-06-16 20:08:44 +0600

    added java faker examples

---
+++ src/main/java/javafaker/Demo.java
@@ -0,0 +1,46 @@
+package javafaker;
+
+import com.github.javafaker.Faker;
+
+public class Demo {
+    protected static Faker faker = new Faker();
+
+    public static void main(String[] args) {
+        System.out.println(generateFakeFirstName());
+        System.out.println(generateFakeLastName());
+        System.out.println(generateFakeEmailAddress());
+        System.out.println(generateArtistName());
+        System.out.println(generateFakeAnimalName());
+        System.out.println(generateFakeProgrammingLanguage());
+    }
+
+    public static String generateFakeFirstName(){
+
+        return "Hello my name is " + faker.name().firstName();
+    }
+
+    public static String generateFakeLastName(){
+
+        return "Surname is: " + faker.name().lastName();
+    }
+
+    public static String generateFakeEmailAddress(){
+
+        return "Email is: " + faker.internet().emailAddress();
+    }
+
+    public static String generateArtistName(){
+
+        return faker.artist().name();
+    }
+
+    public static String generateFakeAnimalName(){
+
+        return faker.animal().name();
+    }
+
+    public static String generateFakeProgrammingLanguage(){
+
+        return faker.programmingLanguage().name();
+    }
+}
